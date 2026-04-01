bdbd352ab658caa5ce004b06a16d630d7b412f7b
Paulo786-source <centenopaulo786@gmail.com>
2026-03-31 22:06:36 -0600

    Estructura inicial del proyecto y CMakeLists

---
+++ PagedArray.h
@@ -0,0 +1,8 @@
+//
+// Created by Paulo Centeno Flores on 3/31/2026.
+//
+
+#ifndef INC_01_PROJECT_PAGEDARRAY_H
+#define INC_01_PROJECT_PAGEDARRAY_H
+
+#endif //INC_01_PROJECT_PAGEDARRAY_H
---
+++ sorter.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Paulo Centeno Flores on 3/31/2026.
+//
